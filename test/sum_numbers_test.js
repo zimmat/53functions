09291fb387f77b0a997cde300808b91d9ab92562
Steve Barnett <steve@naga.co.za>
2016-02-29 17:19:17 +0200

    Fix typos in sum number test

--- test/sum_numbers_test.js
+++ test/sum_numbers_test.js
@@ -3,12 +3,12 @@ var assert = require('assert');
 
 describe("sum_numbers", function(){
 
-    it("should return  14 when called with 5", function(){
+    it("should return 15 when called with 5", function(){
         var result = sum_numbers(5);
-        assert.equal(result, 14)
+        assert.equal(result, 15)
     });
 
-    it("should return 0 when caled with 0", function(){
+    it("should return 0 when called with 0", function(){
         var result = sum_numbers(0);
         assert.equal(result, 0);
     });
