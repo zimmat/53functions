2512e5c2e0571751ec87575f86215169102ff69e
Steve Barnett <steve@naga.co.za>
2016-02-29 17:45:08 +0200

    Update upper_test.js

--- test/upper_test.js
+++ test/upper_test.js
@@ -1,13 +1,13 @@
 var assert = require('assert');
-//TODO - import the module after you created it.
+//TODO: import the module after you created it.
 //var upper = require('../upper');
 
 describe("upper", function(){
 
     it("should uppercase 'ZEBRAS' ", function(){
+        //TODO: call your function and use assert to check the result
         //var result = upper("zebras");
-        var result = "";
-        assert.equals(result, "ZEBRAS");
+        assert.equal(result, "ZEBRAS");
     });
 
 });
