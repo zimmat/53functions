1b128d1e5b0b75883f310ceee8a092169a398c9d
Steve Barnett <steve@naga.co.za>
2016-02-29 16:56:29 +0200

    Update length_test.js

--- test/length_test.js
+++ test/length_test.js
@@ -1,14 +1,14 @@
+var length = require('../length');
 var assert = require('assert');
 
 describe("length", function(){
-    var number_list = require('../length');
 
     it("should return 16 for the string 'zebras are cool!'", function(){
-        assert.equals(length("zebras are cool!"), 16)
+        assert.equal(length("zebras are cool!"), 16)
     });
 
     it("should return 0 for the string ''", function(){
-        assert.equals(length(""), 0);
+        assert.equal(length(""), 0);
     });
 
 });
